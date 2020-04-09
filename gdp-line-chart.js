7f29f6c293042182da15d3a7127366f1779d29e1
Muhammad Ali <46239677+funwithkami@users.noreply.github.com>
2020-04-09 05:18:05 +0500

    Update gdp-line-chart.js

--- gdp-line-chart.js
+++ gdp-line-chart.js
@@ -1,5 +1,53 @@
+<!-- CSS for controls and grid -->
+    <!-- Bootstrap -->
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+      crossorigin="anonymous"
+    />
+    <!-- Fontawesome -->
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <!-- Slider -->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/nouislider@14.2.0/distribute/nouislider.min.css"
+      integrity="sha256-PVqRlNCc4ln4h9kskVZPhqI/EL757EjTWw0OgK2gBmM="
+      crossorigin="anonymous"
+    />
+    <!-- CSS for chart -->
+ <!-- JS for controls -->
+    <!-- Bootstrp -->
+    <script
+      src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+      integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+      integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+      integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+      crossorigin="anonymous"
+    ></script>
+    <!-- Slider -->
+    <script
+      src="https://cdn.jsdelivr.net/npm/nouislider@14.2.0/distribute/nouislider.min.js"
+      integrity="sha256-mJEhSPx2bNn3ZLSPbXX0xNS6gdmEYIhb7NXK0K5edjk="
+      crossorigin="anonymous"
+    ></script>
+    <!-- JS D3 -->
+    <script src="https://d3js.org/d3.v5.min.js"></script>
+    <script src="https://d3js.org/d3-array.v2.min.js"></script>
+
 function renderGDPLineChart() {
-  const dataUrl = "https://cdn.jsdelivr.net/gh/funwithkami/Gdp-Chart/GDP-Data Set.csv";
+//   const dataUrl = "https://cdn.jsdelivr.net/gh/funwithkami/Gdp-Chart/GDP-Data Set.csv";
   const formats = {
     "USD - trillion or hundred billion": {
       tick: d3.format("$~s"),
